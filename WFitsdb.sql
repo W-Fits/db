5227fddb989ee1dd1cabb4fda83dde7a0e191c50
leomosley <leo.mosley2@gmail.com>
2025-02-03 20:29:37 +0000

    fix: typos + `user` naming issue

--- WFitsdb.sql
+++ WFitsdb.sql
@@ -1,47 +1,46 @@
-CREATE TABLE colour_tag(
+CREATE TABLE colour_tag (
     colour_id SERIAL PRIMARY KEY,
     colour_name VARCHAR(35) NOT NULL UNIQUE,
-    colour_value CHAR(7) NOT NULL UNIQUE
+    colour_value VARCHAR(7) NOT NULL UNIQUE
 );
 
-CREATE TABLE catagory_tag(
-    catagory_id SERIAL PRIMARY KEY,
-    catagory_name VARCHAR(35) NOT NULL UNIQUE
+CREATE TABLE category_tag (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(35) NOT NULL UNIQUE
 );
 
-CREATE TABLE size_tag(
+CREATE TABLE size_tag (
     size_id SERIAL PRIMARY KEY,
     size_name VARCHAR(35) NOT NULL UNIQUE
 );
 
-CREATE TYPE enviroment_num AS ENUM ('Warm', 'Cold');
+CREATE TYPE environment_num AS ENUM ('Warm', 'Cold');
 
-CREATE TABLE item(
+CREATE TABLE item (
     item_id SERIAL PRIMARY KEY,
     colour_id INT NOT NULL,
-    catagory_id INT NOT NULL,
+    category_id INT NOT NULL,
     size_id INT NOT NULL,
     item_name VARCHAR(50) NOT NULL,
     item_url TEXT NOT NULL,
     waterproof BOOLEAN,
-    avalable BOOLEAN,
+    available BOOLEAN,
     slot INT NOT NULL,
-    enviroment ENUM,
-    FOREIGN KEY(colour_id) colour_tag(colour_id),
-    FOREIGN KEY(size_id) size_tag(size_id),
-    FOREIGN KEY(catagory_id) catagory_tag(catagory_id)
+    enviroment environment_num NOT NULL,
+    FOREIGN KEY (colour_id) REFERENCES colour_tag(colour_id),
+    FOREIGN KEY (size_id) REFERENCES size_tag(size_id),
+    FOREIGN KEY (category_id) REFERENCES category_tag(category_id)
 );
 
-CREATE TABLE language(
+CREATE TABLE language (
     language_id SERIAL PRIMARY KEY,
     language_name VARCHAR(30)
 );
 
 CREATE TYPE temp_num AS ENUM ('C', 'F');
-
 CREATE TYPE theme_num AS ENUM ('L', 'D');
 
-CREATE TABLE user(
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     language_id INT NOT NULL,
     username VARCHAR(60) NOT NULL,
@@ -49,21 +48,21 @@ CREATE TABLE user(
     firstname VARCHAR(50),
     lastname VARCHAR(50),
     email VARCHAR(150) NOT NULL,
-    set_temp temp_num DEFAULT('C'),
-    set_theme theme_num DEFAULT('L'),
-    FOREIGN KEY(language_id) language(language_id)
+    set_temp temp_num DEFAULT 'C',
+    set_theme theme_num DEFAULT 'L',
+    FOREIGN KEY (language_id) REFERENCES language (language_id)
 );
 
-CREATE TABLE outfit(
+CREATE TABLE outfit (
     outfit_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     createdat DATE NOT NULL,
-    FOREIGN KEY(user_id) user(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE outfit_item(
+CREATE TABLE outfit_item (
     outfit_id INT NOT NULL,
     item_id INT NOT NULL,
-    FOREIGN KEY outfit_id outfit(outfit_id),
-    FOREIGN KEY item_id item(item_id)
+    FOREIGN KEY (outfit_id) REFERENCES outfit(outfit_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES item(item_id)
 );
